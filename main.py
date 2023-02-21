c2f4772089823cbbc4e517421c4094e792918d19
Raoul1808 <raoulthegeek@gmail.com>
2023-02-21 14:47:42 +0100

    Read data from serial port

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import serial
+
+
 def main():
-    print("Hello, World!")
+    # Specifying the port in the Serial constructor will automatically open it
+    port = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=57600,
+        stopbits=1,
+        parity=serial.PARITY_NONE,
+        timeout=0.1)
+
+    while True:
+        data = port.readline()
+        if data != b'':
+            print(bytes(data).hex(' '))
 
 
 if __name__ == '__main__':
