bd922832ed30dda2cf124e8bbc357dc63e17d197
Raoul1808 <raoulthegeek@gmail.com>
2023-03-17 15:19:26 +0100

    Run DatabaseService on a thread

--- corepi/database.py
+++ corepi/database.py
@@ -1,30 +1,59 @@
+import json
+import os
+import requests
 import sqlite3
+from threading import Thread, Event
 
 from corepi.actors.probes import *
 
 
-class DatabaseService:
-    def __init__(self, database):
-        self.db = sqlite3.connect(database)
+class DatabaseService(Thread):
+    def __init__(self, database, timer=60):
+        super(DatabaseService, self).__init__()
+        self.database = database
+        self.send_data = False
+        self.data = {}
+        self.timer = timer
+        self.event = Event()
 
     def update_values_from_probe(self, probe: Probe):
-        query = None
         if isinstance(probe, Co2Probe):
-            query = (f"UPDATE air_data SET"
-                     f"co2_amount = {probe.co2_amount}, "
-                     f"humidity = {probe.humidity}, "
-                     f"temperature = {probe.temperature} "
-                     f"WHERE id = 1;")
+            self.data['co2_amount'] = probe.co2_amount
+            self.data['humidity'] = probe.humidity
+            self.data['temperature'] = probe.temperature
         elif isinstance(probe, VocProbe):
-            query = (f"UPDATE air_data SET"
-                     f"cov_amount = {probe.voc_amount} "
-                     f"WHERE id = 1;")
+            self.data['voc_amount'] = probe.voc_amount
         elif isinstance(probe, ParticleProbe):
-            query = (f"UPDATE air_data SET"
-                     f"pm1_amount = {probe.pm1_amount}, "
-                     f"pm2_5_amount = {probe.pm2_amount}, "
-                     f"pm10_amount = {probe.pm10_amount} "
-                     f"WHERE id = 1;")
-        if query:
-            with self.db:
-                self.db.execute(query)
+            self.data['pm1_amount'] = probe.pm1_amount
+            self.data['pm2_amount'] = probe.pm2_amount
+            self.data['pm10_amount'] = probe.pm10_amount
+
+    def set(self):
+        self.event.set()
+
+    def run(self) -> None:
+        try:
+            db_conn = sqlite3.connect(self.database)
+        except Exception as e:
+            print("Failed to connect to local database:", e)
+            db_conn = False
+        while not self.event.is_set():
+            if self.event.wait(self.timer):
+                break
+            req = "UPDATE air_data SET "
+            for key in self.data.keys():
+                req += f"{key} = {self.data[key]}, "  # Generate SQL request based on
+            req = req[:-2] + " WHERE id = 1;"  # Strip away comma
+            if db_conn:
+                try:
+                    with db_conn:  # Lock the database while writing
+                        db_conn.execute(req)
+                    print(req)
+                except Exception as e:
+                    print("Failed to update internal database:", e)
+            uri = os.getenv("DATABASE_WEBSITE_URI")
+            if uri:
+                req = requests.post(uri, data=json.dumps(self.data))
+                print("Sent data to server. Status code:", req.status_code)
+        if db_conn:
+            db_conn.close()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+
 from corepi import DatabaseService
 from corepi.actors import FrameInterceptor
 from corepi.actors.probes import *
@@ -7,26 +9,34 @@ on_frame = b'\x55\x00\x07\x07\x01\x7a\xf6\x50\xff\xfb\xd8\x80\x30\x02\xff\xff\xf
 
 
 def main():
+    load_dotenv()
+
     interceptor = FrameInterceptor(port='/dev/ttyUSB0')
     dispatcher = ProbeDispatcher()
     dispatcher.register_probe(Co2Probe(0xffd5a80a))
     dispatcher.register_probe(VocProbe(0xffd5a80f))
     dispatcher.register_probe(ParticleProbe(0xffd5a814))
     database = DatabaseService('../respire.db')
+    database.start()
+    print("Database thread started")
 
-    while True:
-        if interceptor.available_frame():
-            frame = interceptor.capture()
-            if not frame:
-                continue
-            print(f"Captured frame of type {hex(frame.device_type)}")
-            if frame.device_type != 0xa5:
-                continue
-            probe = dispatcher.get_probe(frame.sender)
-            if not probe:
-                continue
-            probe.parse(frame.data)
-            database.update_values_from_probe(probe)
+    try:
+        while True:
+            if interceptor.available_frame():
+                frame = interceptor.capture()
+                if not frame:
+                    continue
+                if frame.device_type != 0xa5:
+                    continue
+                probe = dispatcher.get_probe(frame.sender)
+                if not probe:
+                    continue
+                print(f"Captured frame from {type(probe).__name__}")
+                probe.parse(frame.data)
+                database.update_values_from_probe(probe)
+    except KeyboardInterrupt:
+        print("Stopping database service")
+        database.set()
 
 
 if __name__ == '__main__':
